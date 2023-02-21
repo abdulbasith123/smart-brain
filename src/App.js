02c9e85718aed869c0ecf1f1ff9f9f27436b9a3e
basithkalam <basithkalam@gmail.com>
2023-02-21 15:59:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Clarifai from 'clarifai';
+import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const app = new Clarifai.App({
+  apiKey: 'add4230d89f34c659de6c179abd4dcfa'
+});
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = image.width;
+    const height = image.height;
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input })
+    app.models
+      .predict(
+        {
+          id: 'face-detection',
+          name: 'face-detection',
+          version: '6dc7e46bc9124c5c8824be4822abe105',
+          type: 'visual-detector',
+        }, this.state.input)
+
+      .then(response =>
+        this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err))
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg type="cobweb" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Abdul, your current rank is...'}
+            </div>
+            <div className='white f1'>
+                {'#5'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
